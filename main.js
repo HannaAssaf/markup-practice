4f2e23d123f43247263960e3dc6cf627ec3a933d
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-04-10 20:50:17 +0100

    cvb

--- main.js
+++ main.js
@@ -82,3 +82,45 @@ const observerCovers = new IntersectionObserver((entries) => {
 }, optionsCovers);
 
 observerCovers.observe(coversSection);
+const coversSection = document.querySelector("#covers");
+
+const optionsCovers = {
+  root: null,
+  rootMargin: "100px 0px 0px 0px",
+  threshold: 0,
+};
+
+const observerCovers = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log("Section is in view");
+      coversSection.classList.add("animate");
+    } else {
+      console.log("Section is out of view");
+      coversSection.classList.remove("animate");
+    }
+  });
+}, optionsCovers);
+
+observerCovers.observe(coversSection);
+const coversSection = document.querySelector("#covers");
+
+const optionsCovers = {
+  root: null,
+  rootMargin: "100px 0px 0px 0px",
+  threshold: 0,
+};
+
+const observerCovers = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log("Section is in view");
+      coversSection.classList.add("animate");
+    } else {
+      console.log("Section is out of view");
+      coversSection.classList.remove("animate");
+    }
+  });
+}, optionsCovers);
+
+observerCovers.observe(coversSection);
